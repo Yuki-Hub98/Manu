0f2747bf69e239259daa195498c681e152a7f7df
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-09 17:45:13 -0400

    Adiocionando a class Linha

---
+++ manu/src/main/java/com/example/manu/model/entity/Linha.java
@@ -0,0 +1,31 @@
+package com.example.manu.model.entity;
+
+public class Linha {
+    private long id;
+    private String departamento;
+    private String descricao;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
