3e244145e69ee224dd09a5c260f5ebbbf1b31ed1
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-09 17:44:37 -0400

    Adicionando a class grupo

---
+++ manu/src/main/java/com/example/manu/model/entity/Grupo.java
@@ -0,0 +1,31 @@
+package com.example.manu.model.entity;
+
+public class Grupo {
+    private long id;
+    private String familia;
+    private String descricao;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFamilia() {
+        return familia;
+    }
+
+    public void setFamilia(String familia) {
+        this.familia = familia;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
