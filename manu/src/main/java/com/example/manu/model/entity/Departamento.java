12c1e751317dede89f172a33239762915efaeefb
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-09 17:41:22 -0400

    Adicionei getter e setter

--- manu/src/main/java/com/example/manu/model/entity/Departamento.java
+++ manu/src/main/java/com/example/manu/model/entity/Departamento.java
@@ -4,4 +4,19 @@ public class Departamento {
     private long id;
     private String descricao;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
