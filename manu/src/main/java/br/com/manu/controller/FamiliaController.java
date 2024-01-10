36a773b3660cfd5ccb613a6e2c4601c482a5169f
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-10 15:33:39 -0400

    Adicionando controller de Familia

---
+++ manu/src/main/java/br/com/manu/controller/FamiliaController.java
@@ -0,0 +1,27 @@
+package br.com.manu.controller;
+
+import br.com.manu.model.familia.FamiliaResponse;
+import br.com.manu.model.familia.FamiliaResquest;
+import br.com.manu.service.familia.FamiliaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/arvore-produto-familia")
+public class FamiliaController {
+    @Autowired
+    private FamiliaService service;
+
+    @PostMapping
+    public ResponseEntity<FamiliaResponse> create(@RequestBody FamiliaResquest resquest){
+       return ResponseEntity.ok(service.create(resquest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FamiliaResponse>> getAll(){
+        return ResponseEntity.ok(service.getAll());
+    }
+}
