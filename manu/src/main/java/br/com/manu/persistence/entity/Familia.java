f39a13f09ac95c9b12b2e4a9d4445e362639eb39
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-10 15:30:05 -0400

    Criando getter e setter da entidade Familia

--- manu/src/main/java/br/com/manu/persistence/entity/Familia.java
+++ manu/src/main/java/br/com/manu/persistence/entity/Familia.java
@@ -7,4 +7,38 @@ public class Familia {
     private String id;
     private String linha;
     private String descricao;
+
+    public Familia() {
+
+    }
+
+    public Familia(String id, String linha, String descricao) {
+        this.id = id;
+        this.linha = linha;
+        this.descricao = descricao;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLinha() {
+        return linha;
+    }
+
+    public void setLinha(String linha) {
+        this.linha = linha;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
