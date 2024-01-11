b030558ee4222af0632babe0e5f8fd311256b0ff
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-11 08:28:33 -0400

    Adicionando model especificações

---
+++ manu/src/main/java/br/com/manu/model/especificacao/EspecificacaoRequest.java
@@ -0,0 +1,17 @@
+package br.com.manu.model.especificacao;
+
+public class EspecificacaoRequest {
+    private String descricao;
+
+    public EspecificacaoRequest() {
+
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ manu/src/main/java/br/com/manu/model/especificacao/EspecificacaoResponse.java
@@ -0,0 +1,21 @@
+package br.com.manu.model.especificacao;
+
+public class EspecificacaoResponse {
+    private String descricao;
+
+    public EspecificacaoResponse() {
+
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+
+
+}
