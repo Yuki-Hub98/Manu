139baa359e0b05d43e65fee54a84756ad20cf7d7
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-18 16:30:00 -0400

    Add: Adicionando um query find no reposirory

--- manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/LinhaRepository.java
+++ manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/LinhaRepository.java
@@ -1,9 +1,15 @@
 package br.com.manu.persistence.repository.arvoreProduto;
 
+import br.com.manu.persistence.entity.arvoreProduto.Departamento;
 import br.com.manu.persistence.entity.arvoreProduto.Linha;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LinhaRepository extends MongoRepository<Linha, String> {
+    @Query("{'departamento': ?0 }, {'descricao': ?0}")
+    List<Linha> findByname(String departamento, String descricao);
 }
