f22cf790e2a8b1e79661ebbbfb7adde649840aaa
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-18 14:52:59 -0400

    Add: Uma exeption de item duplicado

--- manu/src/main/java/br/com/manu/controller/execeptions/ExceptionsHandlerController.java
+++ manu/src/main/java/br/com/manu/controller/execeptions/ExceptionsHandlerController.java
@@ -8,11 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.management.relation.InvalidRelationIdException;
+import javax.naming.InvalidNameException;
+import javax.naming.directory.InvalidAttributesException;
+import java.io.InvalidObjectException;
 import java.time.Instant;
 
 @ControllerAdvice
 public class ExceptionsHandlerController {
 
+    //Essa validação é para campos vazios
     @ExceptionHandler(NoSuchFieldException.class)
     public ResponseEntity<ExceptionResponse> NotValue (NoSuchFieldException exception, HttpServletRequest request){
         ExceptionResponse error = new ExceptionResponse();
@@ -23,6 +28,18 @@ public class ExceptionsHandlerController {
         error.setPath(request.getRequestURI());
         return ResponseEntity.status(422).body(error);
 
+    }
+
+    //Essa validação é para campos duplicados
+    @ExceptionHandler(InvalidRelationIdException.class)
+    public  ResponseEntity<ExceptionResponse> Dupicate (InvalidRelationIdException exception, HttpServletRequest request){
+        ExceptionResponse error = new ExceptionResponse();
+        error.setTimesTamp(Instant.now());
+        error.setStatus(422);
+        error.setError("Campo Existente");
+        error.setMessage("Essa informação já está cadastrada");
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.status(422).body(error);
     }
 
 }
