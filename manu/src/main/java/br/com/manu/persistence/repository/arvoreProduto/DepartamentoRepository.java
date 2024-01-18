f24f7d56f2d4e28888d3ad8e34d7d8f36486519e
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-18 14:55:57 -0400

    Add: Query para encontrar o valor no banco

--- manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/DepartamentoRepository.java
+++ manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/DepartamentoRepository.java
@@ -2,8 +2,13 @@ package br.com.manu.persistence.repository.arvoreProduto;
 
 import br.com.manu.persistence.entity.arvoreProduto.Departamento;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface DepartamentoRepository extends MongoRepository<Departamento, String> {
+    @Query("{'descricao': ?0 }")
+    Departamento findByname(String valor);
+
 }
