ff8fbfbac620c633d16c468be2868671c28f6c75
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-18 14:49:21 -0400

    Adicionando interface Ex, para um service de Execptions

--- manu/src/main/java/br/com/manu/service/exceptions/ExceptionsNotValue.java
+++
@@ -1,7 +0,0 @@
-package br.com.manu.service.exceptions;
-
-public class NotValue extends RuntimeException{
-            public NotValue(String msg){
-                super(msg);
-            }
-}
---
+++ manu/src/main/java/br/com/manu/service/exceptions/Exe.java
@@ -0,0 +1,7 @@
+package br.com.manu.service.exceptions;
+
+public class ExceptionsNotValue extends RuntimeException{
+            public ExceptionsNotValue(String msg){
+                super(msg);
+            }
+}
