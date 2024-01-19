0c0b3a5b1e631de3d0af498f4d2724c395669651
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-19 08:56:37 -0400

    Att: Adicionando um find no banco para pesquisar familia e descricao

--- manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/GrupoRepository.java
+++ manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/GrupoRepository.java
@@ -1,8 +1,14 @@
 package br.com.manu.persistence.repository.arvoreProduto;
 
 
+import br.com.manu.persistence.entity.arvoreProduto.Familia;
 import br.com.manu.persistence.entity.arvoreProduto.Grupo;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface GrupoRepository extends MongoRepository<Grupo, String> {
+    @Query("{'familia': ?0 }, {'descricao': ?0}")
+    List<Grupo> findByname(String familia, String descricao);
 }
