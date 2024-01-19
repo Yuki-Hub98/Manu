e5b5383797c7598839106374befa74337a72f0bb
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-19 08:45:24 -0400

    Att: Adicionando um find no repositorio para procurar descrição e a linha da familia

--- manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/FamiliaRepository.java
+++ manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/FamiliaRepository.java
@@ -1,7 +1,13 @@
 package br.com.manu.persistence.repository.arvoreProduto;
 
 import br.com.manu.persistence.entity.arvoreProduto.Familia;
+import br.com.manu.persistence.entity.arvoreProduto.Linha;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface FamiliaRepository extends MongoRepository<Familia, String> {
+    @Query("{'linha': ?0 }, {'descricao': ?0}")
+    List<Familia> findByname(String linha, String descricao);
 }
