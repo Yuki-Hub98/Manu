43f4f75ca5a93f0fe328e2bfc12e6b1e8daab022
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-19 08:31:01 -0400

    Att: Adicionando um find para pesquisar a descrição de Especificação

--- manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/EspecificacaoRespository.java
+++ manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/EspecificacaoRespository.java
@@ -1,7 +1,13 @@
 package br.com.manu.persistence.repository.arvoreProduto;
 
+import br.com.manu.persistence.entity.arvoreProduto.Cor;
 import br.com.manu.persistence.entity.arvoreProduto.Especificacao;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface EspecificacaoRespository extends MongoRepository<Especificacao, String> {
+    @Query("{'descricao': ?0 }")
+    List<Especificacao> findByname(String descricao);
 }
