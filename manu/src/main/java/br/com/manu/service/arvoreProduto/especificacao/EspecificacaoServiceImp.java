9166c7a9eb7628ff05d006f5b52b55d075976158
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-19 08:32:49 -0400

    Att: Adicionando uma verificação antes de salvar a descrição no banco

--- manu/src/main/java/br/com/manu/service/arvoreProduto/especificacao/EspecificacaoServiceImp.java
+++ manu/src/main/java/br/com/manu/service/arvoreProduto/especificacao/EspecificacaoServiceImp.java
@@ -2,30 +2,41 @@ package br.com.manu.service.arvoreProduto.especificacao;
 
 import br.com.manu.model.arvoreProduto.especificacao.EspecificacaoRequest;
 import br.com.manu.model.arvoreProduto.especificacao.EspecificacaoResponse;
+import br.com.manu.persistence.entity.arvoreProduto.Departamento;
 import br.com.manu.persistence.entity.arvoreProduto.Especificacao;
 import br.com.manu.persistence.repository.arvoreProduto.EspecificacaoRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.management.relation.InvalidRelationIdException;
 import java.util.ArrayList;
 import java.util.List;
 @Service
 public class EspecificacaoServiceImp implements EspecificacaoService{
     @Autowired
-    EspecificacaoRespository respository;
+    EspecificacaoRespository repository;
 
     @Override
     public EspecificacaoResponse create(EspecificacaoRequest request) {
         Especificacao especificacao = new Especificacao();
         especificacao.setDescricao(request.getDescricao());
-        respository.save(especificacao);
+        find(especificacao).forEach(dep -> {
+            if(dep.getDescricao().equals(especificacao.getDescricao())) {
+                try {
+                    throw new InvalidRelationIdException();
+                } catch (InvalidRelationIdException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        repository.save(especificacao);
         return createResponse(especificacao);
     }
 
     @Override
     public List<EspecificacaoResponse> getAll() {
         List<EspecificacaoResponse> responses = new ArrayList<>();
-        List<Especificacao> especificacoes = respository.findAll();
+        List<Especificacao> especificacoes = repository.findAll();
         if(!especificacoes.isEmpty()){
             especificacoes.forEach(especificacao -> responses.add(createResponse(especificacao)));
         }
@@ -37,5 +48,9 @@ public class EspecificacaoServiceImp implements EspecificacaoService{
         return response;
     }
 
+    private List<Especificacao> find(Especificacao campo){
+        List<Especificacao> find = repository.findByname(campo.getDescricao());
+        return find;
+    }
 
 }
