f5f1c024fbd9c47f7c07ac0a224da828967ebacf
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-19 08:14:16 -0400

    Att: Adicionando uma função find para pesquisar descrição da Cor

--- manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/CorRepository.java
+++ manu/src/main/java/br/com/manu/persistence/repository/arvoreProduto/CorRepository.java
@@ -1,7 +1,13 @@
 package br.com.manu.persistence.repository.arvoreProduto;
 
 import br.com.manu.persistence.entity.arvoreProduto.Cor;
+import br.com.manu.persistence.entity.arvoreProduto.Departamento;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface CorRepository extends MongoRepository<Cor, String> {
+    @Query("{'descricao': ?0 }")
+    List<Cor> findByname(String descricao);
 }
