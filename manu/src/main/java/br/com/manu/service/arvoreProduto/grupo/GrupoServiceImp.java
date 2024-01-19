94566b382dd138b5c9fc90d11f307d3bd0f96eea
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-19 09:00:40 -0400

    Att: Adicionando uma verificação antes de salvar verificando arquivo duplicados

--- manu/src/main/java/br/com/manu/service/arvoreProduto/grupo/GrupoServiceImp.java
+++ manu/src/main/java/br/com/manu/service/arvoreProduto/grupo/GrupoServiceImp.java
@@ -2,11 +2,13 @@ package br.com.manu.service.arvoreProduto.grupo;
 
 import br.com.manu.model.arvoreProduto.grupo.GrupoRequest;
 import br.com.manu.model.arvoreProduto.grupo.GrupoResponse;
+import br.com.manu.persistence.entity.arvoreProduto.Familia;
 import br.com.manu.persistence.entity.arvoreProduto.Grupo;
 import br.com.manu.persistence.repository.arvoreProduto.GrupoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.management.relation.InvalidRelationIdException;
 import java.util.ArrayList;
 import java.util.List;
 @Service
@@ -20,6 +22,15 @@ public class GrupoServiceImp implements GrupoService{
         Grupo grupo = new Grupo();
         grupo.setFamilia(request.getFamilia());
         grupo.setDescricao(request.getDescricao());
+        find(grupo).forEach((li) -> {
+            if (li.getDescricao().equals(grupo.getDescricao()) && li.getFamilia().equals(grupo.getFamilia())) {
+                try {
+                    throw new InvalidRelationIdException();
+                } catch (InvalidRelationIdException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
         repository.save(grupo);
         return createRequest(grupo);
     }
@@ -40,5 +51,9 @@ public class GrupoServiceImp implements GrupoService{
         response.setDescricao(grupo.getDescricao());
         return response;
 
+    }
+    private List<Grupo> find(Grupo campo){
+        List<Grupo> find = repository.findByname(campo.getFamilia(), campo.getDescricao());
+        return find;
     }
 }
