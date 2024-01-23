61b2009fbf64002f7b435c5c812408dffec9f03a
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-23 11:57:13 -0400

    Att: Adicionando uma rota nova search, ao controller, é pesquisa com parametro

--- manu/src/main/java/br/com/manu/controller/arvoreProduto/CorController.java
+++ manu/src/main/java/br/com/manu/controller/arvoreProduto/CorController.java
@@ -2,6 +2,9 @@ package br.com.manu.controller.arvoreProduto;
 
 import br.com.manu.model.arvoreProduto.cor.CorRequest;
 import br.com.manu.model.arvoreProduto.cor.CorResponse;
+import br.com.manu.model.arvoreProduto.departamento.DepartamentoRequest;
+import br.com.manu.model.arvoreProduto.departamento.DepartamentoResponse;
+import br.com.manu.persistence.entity.arvoreProduto.Cor;
 import br.com.manu.service.arvoreProduto.cor.CorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +22,13 @@ public class CorController {
         return ResponseEntity.ok(service.create(request));
 
     }
-
     @GetMapping
     public  ResponseEntity<List <CorResponse>> findAll(){
         return ResponseEntity.ok(service.getAll());
 
+    }
+    @GetMapping("/search")
+    public  ResponseEntity<List<CorResponse>> getDescricao(@RequestParam CorRequest request){
+        return  ResponseEntity.ok(service.getDescricao(request));
     }
 }
