719f165f255cf46cf7ba360732dbd5c65b6e7a66
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-02 17:01:16 -0400

    Add: Nova class criada para nova funcionalidade delete

---
+++ manu/src/main/java/br/com/manu/model/arvoreProduto/grupo/GrupoDel.java
@@ -0,0 +1,26 @@
+package br.com.manu.model.arvoreProduto.grupo;
+
+public class GrupoDel {
+    private String delFam;
+    private String del;
+
+    public GrupoDel() {
+
+    }
+
+    public String getDelFam() {
+        return delFam;
+    }
+
+    public void setDelFam(String delFam) {
+        this.delFam = delFam;
+    }
+
+    public String getDel() {
+        return del;
+    }
+
+    public void setDel(String del) {
+        this.del = del;
+    }
+}
