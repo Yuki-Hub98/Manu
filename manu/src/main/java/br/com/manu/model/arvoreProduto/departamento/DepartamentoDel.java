ef6d077216d72dfc8dc75df754134ee5290f7686
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-02 13:50:53 -0400

    Add: nova class delite

---
+++ manu/src/main/java/br/com/manu/model/arvoreProduto/departamento/DepartamentoDel.java
@@ -0,0 +1,13 @@
+package br.com.manu.model.arvoreProduto.departamento;
+
+public class DepartamentoDel {
+    private String del;
+    public DepartamentoDel() {
+    }
+    public String getDel() {
+        return del;
+    }
+    public void setDel(String del) {
+        this.del = del;
+    }
+}
