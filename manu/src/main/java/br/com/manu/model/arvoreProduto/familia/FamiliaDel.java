a610d589acf70fc9be511709fdb05c4464967b2b
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-02 15:34:24 -0400

    Add: Class del adicionada para a nova funcionalidade del

---
+++ manu/src/main/java/br/com/manu/model/arvoreProduto/familia/FamiliaDel.java
@@ -0,0 +1,25 @@
+package br.com.manu.model.arvoreProduto.familia;
+
+public class FamiliaDel {
+    private String delLinha;
+    private String del;
+
+    public FamiliaDel() {
+    }
+
+    public String getDelLinha() {
+        return delLinha;
+    }
+
+    public void setDelLinha(String delLinha) {
+        this.delLinha = delLinha;
+    }
+
+    public String getDel() {
+        return del;
+    }
+
+    public void setDel(String del) {
+        this.del = del;
+    }
+}
