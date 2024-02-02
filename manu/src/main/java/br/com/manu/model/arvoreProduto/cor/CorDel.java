0ddd567b9b71119a27b51bca11cf72b9a28a924b
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-02 15:56:49 -0400

    Add: Adicionando a class Cor del para o response da nova funcionalidade

---
+++ manu/src/main/java/br/com/manu/model/arvoreProduto/cor/CorDel.java
@@ -0,0 +1,13 @@
+package br.com.manu.model.arvoreProduto.cor;
+
+public class CorDel {
+    private String del;
+    public CorDel() {
+    }
+    public String getDel() {
+        return del;
+    }
+    public void setDel(String del) {
+        this.del = del;
+    }
+}
