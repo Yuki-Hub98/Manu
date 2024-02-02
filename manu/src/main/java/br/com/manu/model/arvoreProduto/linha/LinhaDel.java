c14a2a6d00047ac76a683ceb291f1d91f7796582
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-02 14:54:55 -0400

    Add: nova class delete adiciado no model

---
+++ manu/src/main/java/br/com/manu/model/arvoreProduto/linha/LinhaDel.java
@@ -0,0 +1,25 @@
+package br.com.manu.model.arvoreProduto.linha;
+
+public class LinhaDel {
+    private String depDel;
+    private String del;
+
+    public LinhaDel() {
+    }
+
+    public String getDepDel() {
+        return depDel;
+    }
+
+    public void setDepDel(String depDel) {
+        this.depDel = depDel;
+    }
+
+    public String getDel() {
+        return del;
+    }
+
+    public void setDel(String del) {
+        this.del = del;
+    }
+}
