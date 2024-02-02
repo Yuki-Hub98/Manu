72d214ae6c95922644d17e7ef60d4e0e1567ce0e
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-02 16:12:21 -0400

    Add: Nova class criada para a funcionalidade de delete

---
+++ manu/src/main/java/br/com/manu/model/arvoreProduto/especificacao/EspecificacaoDel.java
@@ -0,0 +1,16 @@
+package br.com.manu.model.arvoreProduto.especificacao;
+
+public class EspecificacaoDel {
+    private String del;
+
+    public EspecificacaoDel() {
+    }
+
+    public String getDel() {
+        return del;
+    }
+
+    public void setDel(String del) {
+        this.del = del;
+    }
+}
